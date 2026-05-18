2c63d795953ce907c39ef8fa5c929f05b78a7866
Riddhi-Halani <riddhi@cueai.co.uk>
2026-05-18 11:25:34 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function () {
+  var toggle = document.querySelector(".nav-toggle");
+  var nav = document.querySelector(".nav-links");
+  if (!toggle || !nav) return;
+
+  toggle.addEventListener("click", function () {
+    var open = nav.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", open ? "true" : "false");
+  });
+
+  nav.querySelectorAll("a").forEach(function (link) {
+    link.addEventListener("click", function () {
+      nav.classList.remove("is-open");
+      toggle.setAttribute("aria-expanded", "false");
+    });
+  });
+})();
